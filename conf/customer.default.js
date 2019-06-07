59a27697dfc45667aa89df5370d7a81b2d6c4984
lheritage <lauraheritage2013@gmail.com>
2019-06-07 08:29:38 -0500

    commit

---
+++ conf/customer.default.js
@@ -0,0 +1,16 @@
+module.exports = {
+	// The configuration settings for your Swagger service.
+	pluginConfig: {
+		'@axway/api-builder-plugin-fn-swagger': {
+			'customer': {
+				// It is possible to override Swagger URI options when constructing
+				// outbound requests from the Swagger plugin.
+				uri: {
+					 protocol: 'http',
+					 host: 'customeraccount',
+					 port: 8080,
+					basePath: '/api'
+				}
+			}
+		}
+	}};
